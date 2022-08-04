ad26585f746601a5491b0a6fdc7bde2fd1ec61cd
蜡笔小新不吃青椒 <88656710+InnocentEyes@users.noreply.github.com>
2022-08-04 16:57:31 +0800

    end learning tcpip

--- main.c
+++ main.c
@@ -2,3 +2,17 @@
 // Created by qiuzelin1 on 2022/8/4.
 //
 
+/**
+ * Learning comes to an end here, thank you for your hard work.
+ * and thank for lky.
+ */
+
+
+#ifdef TCPIP
+#include <stdio.h>
+#define say(name) (printf("%s end learing tcpip",(name)))
+#endif
+
+#ifndef learning_tcpip
+#define TCPIP
+#endif
